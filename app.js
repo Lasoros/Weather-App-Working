e8406aea47ffe26575ca11a201380ee55c8ab60b
Lasoros <ipodtouch_guy@hotmail.com>
2025-01-25 17:22:21 -0500

    Forget Click Event On Button Added

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+const weatherApp = document.querySelector(".weatherApp");
+const city = document.querySelector(".city");
+const weatherWrapper = document.querySelector(".weather-Wrapper");
+const api = "a17959f667e7b1e8813205b90484f33a";
+
+weatherApp.addEventListener("click" , )
